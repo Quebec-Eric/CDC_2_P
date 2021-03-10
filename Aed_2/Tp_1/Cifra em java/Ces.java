71987eaf8944d93d2afa3ba40653b2c31a740624
Eric <quebec.ericjs@gmail.com>
2021-03-10 08:55:28 -0300

    cifra

--- Aed_2/Tp_1/Cifra em java/Ces.java
+++ Aed_2/Tp_1/Cifra em java/Ces.java
@@ -1,72 +1,39 @@
- import java.util.Scanner;
-public class Ces {
-  /**
-   * Funcao da funcao Testar: Verificar se a entrada no teclado e diferente da palavra FIM , com isso retornara um valor booleano
-   * @param String pato que sera analisado se e ou nao a palavra FIM
-   * @return Boolean ; retornara um valor boolean se a palavra e ou nao igual a FIM
-    */
-    public static boolean Testar(String pato){
+public class Ces{
 
-       return (pato.length()==3&&pato.charAt(0)=='F'&&pato.charAt(1)=='I'&&pato.charAt(2)=='M');
+  //Altera a frase com a cifra de cesar
+    public static char crip(char c){
+        return (char)(c+3);
+   }
 
-    }// fim da funcao Testar
-
-    public static void main (String[] args )throws Exception{
-    String resposta="";
-    String entrada = MyIO.readLine();
-    System.out.println(entrada);
-
-      /*
-        o While ira ler do teclado e so ira parar a sua execucao quando a palavra for FIM, fazendo esse teste por meio da
-         funcao Testar
-      */
-       while(Testar(entrada)!=true){
-
-       resposta=  Crip(entrada);
-       String testar = new String(resposta.getBytes("UTF-8"), "ISO-8859-1");
-       System.out.println(testar);
-       entrada = MyIO.readLine();
-
-    }
-
-  } // fim do main
-  /**
-    * Funcao da funcao Crip: ira cripitografar usando a logica de Cesar a string
-    * @param String pato sera a string que sera criptografada
-    * @return String ; retornara  a string criptografada
-     */
-
-     public static String Crip(String pato){
-
-      String cript="";
-      int eric=0;
-      int saber=0;
-     /*
-      for ira cripitografar cara letra da string
-     */
-      for(int i =0;i<pato.length();i++){
-      eric=(int)(pato.charAt(i)+3);
-
-       if (eric >122){
-        eric-= 26;
-          cript+=(char)eric;
+    public static String mudarF(String pato){
+      String resultado = "";
+            for(int i = 0; i < pato.length(); i++){
+                resultado += crip(pato.charAt(i));
+            }
+            return resultado;
       }
-      else {
-          cript+=(char)eric;
-      }
-    }
-      return cript;
-
-    } // fim funcao Crip
 
 
-  } // fim class Ces
+  public static void main (String []args){
 
-  /*
-  String resposta = "";
 
-  String entrada = MyIO.readLine();
+    String[] entrada = new String[1000];
+     int i=0;
+     entrada[i] = MyIO.readLine();
+   while(Testar(entrada[i])!=true) {
 
-  PrintStream outISO = new PrintStream(System.out, true, "ISO-8859-1");
+      i++;
 
-  */
+      entrada[i] = MyIO.readLine();
+   }
+   for (int z = 0; z < i; z++) {
+     MyIO.println(mudarF(entrada[z]));
+     }
+  }
+
+
+
+       public static boolean Testar(String pato){
+    return (pato.length() == 3 && pato.charAt(0) == 'F' && pato.charAt(1) == 'I' && pato.charAt(2) == 'M');
+} // testar
+}
