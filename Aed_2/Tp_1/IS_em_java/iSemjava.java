97b20f429d3d77ffe617f1609fea08cd074195b8
Eric <quebec.ericjs@gmail.com>
2021-03-12 23:20:47 -0300

    is-em-java

--- Aed_2/Tp_1/IS_em_java/iSemjava.java
+++ Aed_2/Tp_1/IS_em_java/iSemjava.java
@@ -1,119 +1,138 @@
-public class main{
-    char[] vogais = {65,69,73,79,85,97,101,105,111,117};
+public class iSemjava{
+    
   /**
    * Funcao da funcao Testar: Verificar se a entrada no teclado e diferente da palavra FIM , com isso retornara um valor booleano
    * @param String pato que sera analisado se e ou nao a palavra FIM
    * @return Boolean ; retornara um valor boolean se a palavra e ou nao igual a FIM
     */
     public static boolean Testar(String pato){
-
        return (pato.length()==3&&pato.charAt(0)=='F'&&pato.charAt(1)=='I'&&pato.charAt(2)=='M');
-
     }// fim da funcao Testar
-
+    
+    // inicio main
      public static void main (String []args){
-
        String entrada = MyIO.readLine();
        while(Testar(entrada)!=true) {
-
      if  (Somvogais(entrada)==true)
           MyIO.print("SIM ");
     else
           MyIO.print("NAO ");
      if (SoComso(entrada)==true)
           MyIO.print("SIM ");
-
     else
            MyIO.print("NAO ");
       if (ContemNi(entrada)==true)
           MyIO.print("SIM ");
     else
            MyIO.print("NAO ");
-
       if (ContenNR(entrada)==true)
-
            MyIO.print("SIM\n");
      else
           MyIO.print("NAO\n");
-
         entrada = MyIO.readLine();
-
        }
-     }
-
+     } // FIm main
+ 
+         /**
+   * Funcao da funcao Somvogais:ira testar se a palavra e ou nao so compostas por vogais
+   * @param String pato que sera analisado se e ou nao uma  composta por vogais
+   * @return Boolean ; retornara um valor boolean 
+    */
+      // inicio Somvogais 
       public static boolean Somvogais(String pato){
-
        boolean saber = true;
-        for (int i =0 ;i<pato.length();i++  ) {
-
+        for (int i =0 ;i<pato.length();i++) {
                 if(!sVogal(pato.charAt(i))){
-
                   saber = false;
                 }
           }
-
           return saber;
-
-        }
-
-        public static boolean sVogal(char eric) {
+        } // Fim SOmvogais
+ 
+      /**
+   * Funcao da funcao sVogais:ira testar se cade Char e ou nao uma  vogal
+   * @param char eric a ser analisado se e ou nao uma vogal
+   * @return Boolean ; retornara um valor boolean 
+    */
+       //inicio sVogal
+         public static boolean sVogal(char eric) {
        boolean sVogal=false;
        if(eric=='A'||eric=='a'||eric=='E'||eric=='e'||eric=='I'||eric=='i'||eric=='O'||eric=='o'||eric=='U'||eric=='u')
        {
            sVogal=true;
        }
        return sVogal;
-   }
-
+   } // Fim sVogal
+ 
+    /**
+   * Funcao da funcao soComso:ira testar se cade a palavra so contem ou nao consoantes
+   * @param String eric a ser analisado se e ou nao uma vogal
+   * @return Boolean ; retornara um valor boolean 
+    */
+    // inicio SoComso
     public static boolean SoComso(String pato){
       boolean saber = true;
-       for (int i =0 ;i<pato.length()-1;i++  ) {
-
+       for (int i =0 ;i<pato.length()-1;i++) {
                if(sVogal(pato.charAt(i))||!contemletra(pato.charAt(i))){
-
-                 saber = false;
+                saber = false;
                }
          }
      return saber;
+    } // fim SoComso
 
-
-    }
-
+   /**
+   * Funcao da funcao contemNI:ira testar se a palavra contem ou nao numeros 
+   * @param String pato , a qual ira ser analisada se comtem numeros ou nao
+   * @return Boolean ; retornara um valor boolean 
+    */
+    // inicio ContemNi
     public static boolean ContemNi (String pato){
       boolean saber = true;
        for (int i =0 ;i<pato.length();i++  ) {
-
                if(pato.charAt(i)=='.'||pato.charAt(i)==','||contemletra(pato.charAt(i))){
-
                  saber = false;
-
                }
          }
      return saber;
+    } // fim COmtemNi
 
-    }
-
+    /**
+   * Funcao da funcao contemletra ira analisar se o char e uma letra ou nao 
+   * @param char eric , o qual ira sofrer o teste de saber se e letra ou nao
+   * @return Boolean ; retornara um valor boolean 
+    */
+   // inico contemletra
     public static boolean contemletra(char eric)
  {
      boolean saber=false;
      if(eric>='A'&&eric<='Z'||eric>='a'&&eric<='z')
          saber=true;
      return saber;
- }
-
+ }// fim contemletra
+ 
+   /**
+   * Funcao da funcao tsteSupremo , e o teste que verificar por fim a string , sendo supremo
+   * @param String pato , a string que ira ser testa 
+   * @return Boolean ; retornara um valor boolean 
+    */
+ // inicio testeSupremo 
  public static boolean testeSupremo(String pato){
   boolean saber = false;
   int testeSupremo =0;
   for (int i =0 ;i<pato.length();i++  ) {
-
           if((pato.charAt(i) >= '0' && pato.charAt(i) <= '9')){
            saber =true;
       }
-
   }
     return saber;
- }
-
+ }// fim teste supremo 
+ 
+ /**
+   * Funcao da funcao ContenNR ira verificar se a string contem numeros reais
+   * @param String pato , string que ira sofrer o teste
+   * @return Boolean ; retornara um valor boolean 
+    */  
+  //inicio contenNR
  public static boolean ContenNR(String pato){
 
    boolean saber = false;
@@ -127,7 +146,7 @@ public class main{
       }
   return saber;
 
- }
+ }// fim ContenNR
 
 
 
